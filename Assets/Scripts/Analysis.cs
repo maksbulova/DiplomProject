4e527637539780def05a8bde91cdc7bcb886e5a3
maksbulova <maks.bulova@gmail.com>
2021-04-04 00:08:20 +0300

    Queueueue
    Added priority queue

--- Assets/Scripts/Analysis.cs
+++ Assets/Scripts/Analysis.cs
@@ -1,21 +1,109 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+
+public struct PriorityQueue<T>
+{
+    private Dictionary<T, float> priorityQueue;
+
+
+    public int Count
+    {
+        get { return priorityQueue.Count; }
+    }
+
+    public void Enqueue(T element, float priority)
+    {
+        if (priorityQueue.ContainsKey(element))
+        {
+            priorityQueue[element] = priority; // TODO надо ли перезаписывать приоритет или сравнивать с текущим
+        }
+        else
+        {
+            priorityQueue.Add(element, priority);
+        }
+    }
+
+
+    public KeyValuePair<T, float> Dequeue(bool maxPriority)
+    {
+        KeyValuePair<T, float> priorityElement;
+        if (maxPriority)
+        {
+            priorityElement = GetMax();
+        }
+        else
+        {
+            priorityElement = GetMin();
+        }
+        
+        priorityQueue.Remove(priorityElement.Key);
+        return priorityElement;
+    }
+
+    public KeyValuePair<T, float> Peek(bool maxPriority)
+    {
+        if (maxPriority)
+        {
+            return GetMax();
+        }
+        else
+        {
+            return GetMin();
+        }
+        
+    }
+
+    private KeyValuePair<T, float> GetMax()
+    {
+        return priorityQueue.Aggregate((m, n) => m.Value > n.Value ? m : n);   // TODO смысл копипаста понял, но не тестил
+    }
+
+    private KeyValuePair<T, float> GetMin()
+    {
+        return priorityQueue.Aggregate((m, n) => m.Value < n.Value ? m : n);
+    }
+
+}
 
 public static class Analysis
 {
+
+
+    /*
     public static LinkedList<Node> AStar(Graph graph, Node start, Node finish)
     {
         LinkedList<Node> way = new LinkedList<Node>();
 
+        // узел для рассмотрения и его оценка
+        List<(Node, float)> open = new List<(Node, float)>() { (start, 0) };
 
+        // рассмотренный узел и узел откуда мы пришли в рассмотренный
+        List<(Node, Node)> closed = new List<(Node, Node)>();
+
+        while (open.Count > 0)
+        {
+            foreach (var item in collection)
+            {
+
+            }
+        }
 
         return null;
     }
+    */
+
+    public static LinkedList<Node> BFS(Graph graph, Node start, Node finish)
+    {
+
+    }
+
 
     public static void MaxFlow(Graph graph, Node start, Node finish)
     {
-        // Форда-Фалкерсона
+        // Форда-Фалкерсона 
 
         // 1 обнуляємо усі потоки
         foreach (List<(Node, Edge)> node in graph.nodeList.Values)
